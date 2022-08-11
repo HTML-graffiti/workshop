c3c90b17c1cf83436fd6d3e4b673223204039442
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-11 16:27:53 +0900

    Update order.php

--- order.php
+++ order.php
@@ -246,14 +246,14 @@ a:hover {
 <input id="time" type="text" name="time" placeholder="ワークショップ希望開始時間" value="<?php if( !empty($_POST['time']) ){ echo $_POST['time']; } ?>" required>
 </hr>
 <p><br/><u>希望するワークショップ</u></p>
-HTMLの基本（所要時間：約1時間）<br/>
+<p><br/>HTMLの基本（所要時間：約1時間）</p>
 <select name="html">
 <option value="無回答">- 選択する -</option>
 <option value="希望する">希望する</option>
 <option value="希望しない">希望しない</option>
 </select>
 
-CSSの基本（所要時間：約1時間）<br/>
+<p><br/>CSSの基本（所要時間：約1時間）</p>
 <select name="css">
 <option value="無回答">- 選択する -</option>
 <option value="希望する">希望する</option>
